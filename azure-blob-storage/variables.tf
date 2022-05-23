88bc6da8972409666b633e7ab68ca61c3f0268cc
maciejewskimarcin <92676129+maciejewskimarcin@users.noreply.github.com>
2022-05-23 19:44:57 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,41 @@
+variable "name_marcin_resources" {
+  desctription = "name of marcin resource"
+  type = string
+  default = "marcin-resources"
+}
 
+variable "location" {
+  desctription = "location"
+  type = string
+  default = "West Europe"
+}
+
+variable "name_storage_account" {
+  desctription = "name of storage account"
+  type = string
+  default = "marcinstorage"
+}
+
+variable "account_tier" {
+  desctription = "account tier"
+  type = string
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  desctription = "type"
+  type = string
+  default = "LRS"
+}
+
+variable "name_storage_container" {
+  desctription = "name of storage container"
+  type = string
+  default = "content"
+}
+
+variable "access_container_type" {
+  desctription = "type"
+  type = string
+  default = "private"
+}
